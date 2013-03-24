0d1578fad5bd1ddc4d23467dd397ebb8d24d883c
Chmouel Boudjnah <chmouel@chmouel.com>
2013-03-24 18:05:37 +0100

    PEP8

--- kslegacyv1/core.py
+++ kslegacyv1/core.py
@@ -14,9 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import webob
-import uuid
 
-from keystone import identity
 from keystone import config
 from keystone import token
 from keystone import exception
